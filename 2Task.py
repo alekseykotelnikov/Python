b2afec764d1925026cb978ce688c9ee2904815f0
aleksey <aleksey7008795@yandex.ru>
2022-04-15 20:34:33 +0300

    Second Task

---
+++ 2Task.py
@@ -0,0 +1,8 @@
+# Найти максимальное из пяти чисел
+
+numbers = [1, 2, 6, 4, 5]
+print('Даны следующие числа:')
+for i in numbers:
+    print(i, end=' ')
+print()
+print('Максимальное число, это: ', max(numbers))
