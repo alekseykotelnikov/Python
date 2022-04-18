d99dcb5add6b140d1d62af222753c2138dc812cd
aleksey <aleksey7008795@yandex.ru>
2022-04-18 21:12:03 +0300

    Задача выполнена через 3 цикла for

--- 7Task.py
+++ 7Task.py
@@ -1,11 +1,20 @@
-# Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
-import os
-os.system("cls")
+#Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
 
-x = 0
-y = 1
-z = 2
-left = not (x or y or z)
-right = not x and not y and not z
-statement = left == right
-print (statement)
+x = [True, False]
+y = [True, False]
+z = [True, False]
+check = 1
+
+for i in range (0, 2):
+    for j in range (0, 2):
+        for k in range (0, 2):
+            if (not (x[i] or y[j] or z[k]) == (not x[i] and not y[j] and not z[k])):
+                print(f'Выражение из условия - True')
+            else:
+                print(f'Выражение из условия - False')
+                check = 0
+
+if (check == 1):
+    print('Выражение ИСТИННО!')
+else:
+    print('Выражение ЛОЖНО!')
