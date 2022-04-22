767920b2208841a0cfdfe9e78940fe06398fe81a
aleksey <aleksey7008795@yandex.ru>
2022-04-22 21:19:44 +0300

    Add second

--- 19Task.py
+++ 19Task.py
@@ -5,7 +5,7 @@ os.system('cls')
 import datetime
 
 def rand_number():
-    return datetime.datetime.now().microsecond % 10 # выдает дату часы минуты секунды микросекунды сейчас
+    return datetime.datetime.now().second % 10 # выдает дату часы минуты секунды микросекунды сейчас
 
 rand = rand_number()
 print(f'Random number is: {rand}')
