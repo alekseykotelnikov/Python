f8dd0c3cfec2df0c8b001730f68c9db83df84fd1
aleksey <aleksey7008795@yandex.ru>
2022-04-27 23:38:33 +0300

    comleted with ** and /

--- 30task_pi.py
+++ 30task_pi.py
@@ -1,13 +1,12 @@
 # 30. Вычислить число pi c заданной точностью d
 # Пример: при d = 0.001,  pi = 3.142. 10**-1 <= d10 <= 10**-10
-
-
 import os
 from math import *
 os.system("cls")
 
+print(f'Число Пи: {pi}')
 num = int(input('Введите какое количество знаков после запятой нужно выводить: '))
 d = 10**(-num)
 print(f'Точность d = {d}')
-print(f'Пи = {round(pi, num)}')
-
+a = (floor(pi * 10**num)) / 10**num
+print(f'Пи = {a}')
