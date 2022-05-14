a30871ecca4cb7f1732c0af74ac68c1b529ac98b
aleksey <aleksey7008795@yandex.ru>
2022-05-14 22:41:42 +0300

    completed with files Task Code Decode Text

---
+++ 42Task.py
@@ -0,0 +1,55 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+# Пример:
+# На сжатие:
+# Входные данные:
+# WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW
+# Входные данные:
+# 12W1B12W3B24W1B14W
+
+import os
+os.system('cls')
+
+def encode_file(my_text):  
+    str_code = ''
+    count = 1       
+    for i in range(len(my_text)):
+        if i < len(my_text)-1:
+            if my_text[i] == my_text[i + 1]:
+                count += 1
+            else:
+                str_code += str(count) + my_text[i]
+                count = 1
+        else:
+            str_code += str(count) + my_text[i]
+    return str_code
+
+
+
+def decode_file(strc):
+    count = ''
+    result = ''
+    for i in strc:
+        if i.isdigit():
+            count += i
+        else:
+            result += i * int(count)
+            count = ''
+    return result
+
+text = 'WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW'
+print(f'Предоставлен следующий текст: \n{text}')
+with open('42_decode.txt', 'w') as data:
+    data.write(text)
+
+with open('42_decode.txt', 'r') as data:
+    my_text = data.read()
+
+strc = encode_file(my_text)
+print(f'Сжатый текст будет вот такого вида: \n{strc}') # Проверка магии
+
+with open('42_code2.txt', 'w') as data:
+    data.write(strc)
+
+total = decode_file(strc)
+print(f'Восстановленный текст будет следующего вида: \n{total}') # проверка второй магии
