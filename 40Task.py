bdfca00f25801406c7ed2b5f149b0eaefae4bb53
aleksey <aleksey7008795@yandex.ru>
2022-05-16 19:55:13 +0300

    add hard bot

--- 40Task.py
+++ 40Task.py
@@ -12,7 +12,10 @@ os.system('cls')
 import random
 from tokenize import String
 
-greeting = ('Чтобы начать голодные игры, необходимо решить, В какой режим Вы хотите играть? Если с Игроком2: введите 1, если с Ботом введите 2')
+greeting = ('Чтобы начать голодные игры, необходимо решить, \
+    \nВ какой режим Вы хотите играть? \
+        \nЕсли с Игроком2: введите 1, если с Ботом введите 2 \
+            \nесли хотите почувствовать себя ничтожеством - введите 3 (BigBoss)')
 n = 21
 m = 3          
 
@@ -50,6 +53,26 @@ def play_game_bot(n, m, players, messages, number_player):
         number_player +=1
     return players[not number_player%2]
 
+def play_game_bot_hard(n, m, players, messages, number_player):
+    while n > 0:
+        if number_player%2 != 0:
+            if n > m:
+                move = n - ((n // (m+1)) * (m+1))
+            else:
+                move = n
+            print(f'Я беру {move} конфет')
+        else:
+            print(f'{players[number_player%2]}, {random.choice(messages)}')
+            move = int(input())
+            while move > n or move > m:
+                print(f'Это слишком много, можно взять не более {m} конфет, у нас всего {n} конфет')
+                move = int(input())
+        n = n - move
+        if n > 0: print(f'Осталось {n} конфет')
+        else: print('Все конфеты разобраны.')
+        number_player +=1
+    return players[not number_player%2]
+
 def choice(players):
     count = 0
     print(f'{players[0]} А теперь жеребьевка!!! Выберите: если орел введите 1, если решка 2')
@@ -67,19 +90,29 @@ def choice(players):
     return count
 
 print(greeting)
-num = int(input())
-if num == 1:
+num = input()
+while num not in '123':
+    print('Введено неверное значение. Выберите 1, 2 или 3')
+    num = input()
+if num == '1':
     player1 = input('Давайте познакомися. Первый игрок, как к Вам можно обращаться?\n')
     player2 = input('Второй игрок, и Вы представьтесь, пожалуйста\n')
     players = [player1, player2]
     number_player = choice(players)
     winer = play_game(n, m, players, messages, number_player)
-if num == 2:
+if num == '2':
     player1 = input('Давайте познакомися. Первый игрок, как к Вам можно обращаться?\n')
     print(f'Привет {player1}!!! А меня зовут Бот.')
     bot = 'Бот'
     players = [player1, bot]
     number_player = choice(players)
     winer = play_game_bot(n, m, players, messages, number_player)
+if num == '3':
+    player1 = input('Давайте познакомися. Первый игрок, как к Вам можно обращаться?\n')
+    print(f'Привет {player1}!!! А меня зовут BigBoss. И я здесь, чтобы унизить тебя!!!')
+    bot = 'BigBoss'
+    players = [player1, bot]
+    number_player = choice(players)
+    winer = play_game_bot_hard(n, m, players, messages, number_player)
 
-print(f'Поздравляю! В этот раз победил {winer}! Ему достаются все конфеты!')
+print(f'Ииииии победитель, это {winer}! Ему достаются все конфеты!')
