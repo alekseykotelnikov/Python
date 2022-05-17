696f0157f7952ff72f6f2693d5eb3ba27ace8288
aleksey <aleksey7008795@yandex.ru>
2022-05-17 22:15:20 +0300

    small version added

--- 16.1Task.py
+++ 16.1Task.py
@@ -5,10 +5,12 @@ import random
 os.system("cls")
 
 n = random.randint(6, 16)
-print(n)
+# print(n)
+# n = 8
+# massive = []
 
-massive = []
-
-for i in range(1, n+1):
-    massive.append(1+(1/i)**i)
-print(f'{sum(massive):.2f}')
+# for i in range(1, n+1):
+#     massive.append(1+(1/i)**i)
+# print(f'{sum(massive):.2f}')
+# number = [(1+(1/i)**i) for i in range(1, n+1)]
+print(f'{sum([(1+(1/i)**i) for i in range(1, n+1)]):.2f}')
