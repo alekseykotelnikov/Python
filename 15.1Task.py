5229caef62d30ff87ae5e63915a321c235670334
aleksey <aleksey7008795@yandex.ru>
2022-05-17 22:04:26 +0300

    small version added

--- 15.1Task.py
+++ 15.1Task.py
@@ -1,11 +1,20 @@
-def multy(a):
-    if a == 1:
-        return 1
+import os
+os.system('cls')
+from itertools import *
+import operator
+from random import *
+
+# def multy(a):
+#     if a == 1:
+#         return 1
     
-    return a * multy(a-1)
+#     return a * multy(a-1)
 
-n = 10
+# n = 10
 
-print("Sequence for ")
-for i in range(1, n+1):
-    print(multy(i), end=' ')
+# print("Sequence for ")
+# for i in range(1, n+1):
+#     print(multy(i), end=' ')
+
+n = randint(4, 9)
+print(*list(accumulate([x for x in range(1, n + 1)], operator.mul)))
