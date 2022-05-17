fdd027c7879098253d6aae3cc9fea11aaa14e060
aleksey <aleksey7008795@yandex.ru>
2022-05-17 21:50:33 +0300

    small version added

--- 11Task.py
+++ 11Task.py
@@ -4,19 +4,20 @@
 import os
 os.system('cls')
 
-def try_parse_int():
-    while True:
-        try:
-            console_input = int(input('Enter the N number: '))
-            if 3 <= console_input <= 8:
-                return int(console_input)
-        except ValueError:
-            print('Wrong number. Try again')
-def get_degree(n):
-    print('Список из {} членов последовательности, будет: '.format(n))
-    return [((-3)**i) for i in range(n)]
+# def try_parse_int():
+#     while True:
+#         try:
+#             console_input = int(input('Enter the N number: '))
+#             if 3 <= console_input <= 8:
+#                 return int(console_input)
+#         except ValueError:
+#             print('Wrong number. Try again')
+# def get_degree(n):
+#     print('Список из {} членов последовательности, будет: '.format(n))
+#     return [((-3)**i) for i in range(n)]
+# n = try_parse_int()
+# print(get_degree(n))
 
-print(list((-3)**i for i in range(int(input()))))
+print(list((-3)**i for i in range(int(input('Введите N: ')))))
 
-n = try_parse_int()
-print(get_degree(n))
+
