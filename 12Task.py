c66ce54459bd0bd7744fd758f5f2c786195184e0
aleksey <aleksey7008795@yandex.ru>
2022-05-17 21:51:24 +0300

    small version added

--- 12Task.py
+++ 12Task.py
@@ -4,24 +4,21 @@
 import os
 os.system('cls')
 
-def try_parse_int():
-    while True:
-        try:
-            console_input = int(input('Enter the N number: '))
-            if 3 <= console_input <= 8:
-                return int(console_input)
-        except ValueError:
-            print('Wrong number. Try again')
+# def try_parse_int():
+#     while True:
+#         try:
+#             console_input = int(input('Enter the N number: '))
+#             if 3 <= console_input <= 8:
+#                 return int(console_input)
+#         except ValueError:
+#             print('Wrong number. Try again')
 
+# def get_dictionary(n):
+#     print('Для натурального N словарь индекс-значение, ', end="")
+#     print('состоящий из элементов последовательности 3n + 1, будет: ')
+#     return {a: 3 * a + 1 for a in range(1, n + 1)}
 
-
-def get_dictionary(n):
-    print('Для натурального N словарь индекс-значение, ', end="")
-    print('состоящий из элементов последовательности 3n + 1, будет: ')
-    return {a: 3 * a + 1 for a in range(1, n + 1)}
-
-n = try_parse_int()
-print(get_dictionary(n))
-
+# n = try_parse_int()
+# print(get_dictionary(n))
 
 print({a: 3*a + 1 for a in range(1, int(input('Введите N: ') ) + 1)})
