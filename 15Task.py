71a05a1bf768dbfea90dc2fd5ab938fad2488f7c
aleksey <aleksey7008795@yandex.ru>
2022-05-17 22:03:48 +0300

    small version added

--- 15Task.py
+++ 15Task.py
@@ -4,26 +4,30 @@
 
 import os
 os.system('cls')
+from itertools import *
+import operator
+from random import *
+# def try_parse_int():
+#     while True:
+#         try:
+#             console_input = int(input('Enter the N number: '))
+#             if 3 <= console_input <= 8:
+#                 return int(console_input)
+#         except ValueError:
+#             print('Wrong number. Try again')
 
-def try_parse_int():
-    while True:
-        try:
-            console_input = int(input('Enter the N number: '))
-            if 3 <= console_input <= 8:
-                return int(console_input)
-        except ValueError:
-            print('Wrong number. Try again')
+# def factorial(n):
+#     list = [1]
+#     for i in range(2, n + 1):
+#         list.append(i * list[i - 2])
+#     return list
 
-def factorial(n):
-    list = [1]
-    for i in range(2, n + 1):
-        list.append(i * list[i - 2])
-    return list
+# n = try_parse_int()
+# list = factorial(n)
+# print(list)
 
-n = try_parse_int()
-list = factorial(n)
-print(list)
-
+n = randint(4, 9)
+print(*list(accumulate([x for x in range(1, n + 1)], operator.mul)))
 
 
 
