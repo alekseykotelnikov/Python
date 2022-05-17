575e312fae16b15f58e6e8700ce807672a8e1d43
aleksey <aleksey7008795@yandex.ru>
2022-05-17 21:52:06 +0300

    small version added

--- 14Task.py
+++ 14Task.py
@@ -2,21 +2,20 @@
 import os
 os.system('cls')
 
-def try_parse_float():
-    while True:
-        try:
-            console_input = float(input('Enter the number: '))
-            return console_input
-        except ValueError:
-            print('Wrong number. Try again')
+# def try_parse_float():
+#     while True:
+#         try:
+#             console_input = float(input('Enter the number: '))
+#             return console_input
+#         except ValueError:
+#             print('Wrong number. Try again')
 
-
-n = try_parse_float()
-n = str(n)
-n = (n.replace('.', ''))
-n = list(n)
-result = sum(map(int, n))
-print(n)
-print(result)
+# n = try_parse_float()
+# n = str(n)
+# n = (n.replace('.', ''))
+# n = list(n)
+# result = sum(map(int, n))
+# print(n)
+# print(result)
 
 print(sum(map(int, list(str(float(input('Введите число с плавающей точкой: '))).replace('.', '')))))
